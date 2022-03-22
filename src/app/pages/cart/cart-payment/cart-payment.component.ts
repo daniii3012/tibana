a404fe93102711fdc28c6af681234d504eaecdd6
Daniel Montiel <daniel_301999@hotmail.com>
2022-03-21 19:22:13 -0500

    Carrito de compras

--- src/app/pages/cart/cart-payment/cart-payment.component.ts
+++ src/app/pages/cart/cart-payment/cart-payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsService } from 'src/app/services/items/items.service';
 
 @Component({
   selector: 'app-cart-payment',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPaymentComponent implements OnInit {
 
-  constructor() { }
+  personalInfoToggle = true;
+  shippingAddressToggle = false;
+  paymentInfoToggle = false;
+  canPayToggle = false;
+
+  licores: any;
+
+  constructor(
+    private itemsService: ItemsService
+  ) { }
 
   ngOnInit(): void {
+    this.getLicores();
+  }
+
+  getLicores() {
+    this.itemsService.getLicores().subscribe(
+      data => {
+        this.licores = data;
+        console.log(this.licores);
+      }
+    );
+    
+  }
+
+  original_state() {
+    this.personalInfoToggle = true;
+    this.shippingAddressToggle = false;
+    this.paymentInfoToggle = false;
+    this.canPayToggle = false;
+  }
+
+  personal_info() {
+    this.personalInfoToggle = false;
+    this.shippingAddressToggle = true;
+    this.paymentInfoToggle = false;
+    this.canPayToggle = false;
+  }
+
+  shipping_address() {
+    this.personalInfoToggle = false;
+    this.shippingAddressToggle = false;
+    this.paymentInfoToggle = true;
+    this.canPayToggle = false;
+  }
+
+  payment_info() {
+    this.personalInfoToggle = false;
+    this.shippingAddressToggle = false;
+    this.paymentInfoToggle = false;
+    this.canPayToggle = true;
   }
 
 }
